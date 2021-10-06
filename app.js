8e7a6551818b10dd30271431a31abf8edebe450d
Anand Raj <anandraj.solutions@gmail.com>
2021-10-06 21:32:36 +0530

    delete functionality added

--- app.js
+++ app.js
@@ -53,7 +53,11 @@ app.put('/campgrounds/:id', async (req,res) => {
     const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground});
     res.redirect(`/campgrounds/${campground._id}`);
 });
-
+app.delete('/campgrounds/:id', async (req,res) => {
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+});
 /* app.get('/makecampground',async (req,res) => {
     const camp = new Campground({ title: 'My Backyard', description:'Cheap camping'});
     await camp.save();
