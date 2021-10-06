a24f4afad54d3e30282160c8cf29497252ec4e12
Anand Raj <anandraj.solutions@gmail.com>
2021-10-06 21:24:55 +0530

    Update functionality added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app =express();
 const path = require('path');
 const mongoose =require('mongoose');
+const methodOverride= require('method-override');
 const Campground = require('./models/campground');
 
 
@@ -22,6 +23,7 @@ app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 
 app.get('/',(req,res) => {
     res.render('home');
@@ -42,7 +44,15 @@ app.get('/campgrounds/:id',async (req,res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show',{campground});
 });
-
+app.get('/campgrounds/:id/edit',async (req,res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit',{campground});
+});
+app.put('/campgrounds/:id', async (req,res) => {
+    const {id} =req.params;
+    const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`);
+});
 
 /* app.get('/makecampground',async (req,res) => {
     const camp = new Campground({ title: 'My Backyard', description:'Cheap camping'});
